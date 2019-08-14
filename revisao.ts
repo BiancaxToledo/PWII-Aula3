ad5d260337f256aae69e6e8799423324f983a360
BiancaxToledo <bianca.dharas2@gmail.com>
2019-08-14 20:25:09 -0300

    Aula 3

---
+++ revisao.ts
@@ -0,0 +1,9 @@
+var resultado = 0;
+var número1 = 5;
+var número2 = 5;
+var número3 = 5;
+
+resultado = número1 + número2 + número3;
+resultado = resultado;
+
+console.log("O resultado é:  " + resultado);

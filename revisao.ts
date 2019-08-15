c710dd36d2eb12cfe88ea345086ebc9bf2080fb9
BiancaxToledo <bianca.dharas2@gmail.com>
2019-08-14 21:45:49 -0300

    Criação de função

--- revisao.ts
+++ revisao.ts
@@ -1,9 +1,16 @@
-var resultado = 0;
-var número1 = 5;
-var número2 = 5;
-var número3 = 5;
+{
+let n1 :number = 5;
+let n2 :number = 5;
+let n3 :number = 5;
 
-resultado = número1 + número2 + número3;
-resultado = resultado;
+//let media: number = (nota1 + nota2 + nota3)/3
 
-console.log("O resultado é:  " + resultado);
+    let fMedia = function (nota1:number, nota2:number, nota3:number) : number
+{
+    return(nota1 + nota2 + nota3) / 3
+}
+
+
+
+console.log(`A média é:  ${fMedia(n1,n2,n3)}`);
+}

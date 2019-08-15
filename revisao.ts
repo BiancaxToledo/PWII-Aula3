d30d485132f7485a58ffaf577001f45a4af22f75
BiancaxToledo <bianca.dharas2@gmail.com>
2019-08-14 22:13:04 -0300

    Valor padrão (função)

--- revisao.ts
+++ revisao.ts
@@ -1,14 +1,14 @@
 {
 let n1 :number = 5;
 let n2 :number = 5;
-let n3 :number = 5;
+
 
 //let media: number = (nota1 + nota2 + nota3)/3
 
-    let fMedia = (nota1:number, nota2:number, nota3:number) => (nota1 + nota2 + nota3) / 3
+    let fMedia = (nota1:number, nota2:number, nota3:number=7) => (nota1 + nota2 + nota3) / 3
 
 
 
 
-console.log(`A média é:  ${fMedia(n1,n2,n3)}`);
+console.log(`A média é:  ${fMedia(n1,n2)}`);
 }
